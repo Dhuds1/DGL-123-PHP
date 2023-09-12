961459eafa36ed1b50c3131ef4e5efd59a05272b
Dhuds1 <dhudson1@northislandcollege.ca>
2023-09-11 19:06:43 -0700

    added files
    created the folder and sub folder, added an index page and gave it some basic boiler code

---
+++ laracast/assignment1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laracast Assignment 1</title>
+</head>
+<body>
+    
+    <H1>Hello World!</H1>
+
+</body>
+</html>
